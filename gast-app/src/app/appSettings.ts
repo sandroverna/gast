94b9fb023216cc4eea906018c0263790203b3e4b
alessandro verna <a.verna@synclab.it>
2017-09-22 11:35:10 +0200

    change: chat url

--- gast-app/src/app/appSettings.ts
+++ gast-app/src/app/appSettings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
     public static API_ENDPOINT = 'http://localhost:9000';
-    public static WS_CHAT = 'ws://localhost:8080/gastwebsocket';
+    public static WS_CHAT = 'ws://localhost:8088/gast-ws-chat';
 
     static getHeaders(): Headers {
         return new Headers({ 'Content-Type': 'application/json' });
